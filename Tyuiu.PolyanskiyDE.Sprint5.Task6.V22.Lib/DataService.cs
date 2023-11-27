edb37aea8477935f3eadc9c1b0a77719421eff22
Tyuiu.PolyanskiyDE <danilp.2005@mail.ru>
2023-11-28 01:38:19 +0500

    Task6

---
+++ Tyuiu.PolyanskiyDE.Sprint5.Task6.V22.Lib/DataService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.PolyanskiyDE.Sprint5.Task6.V22.Lib
+{
+    public class DataService : ISprint5Task6V22
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length - 1; i++)
+                    {
+                        for (int j = 1; j < line.Length; j++)
+                        {
+                            if (line[i] == 'м' && line[j] == 'м')
+                            {
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.PolyanskiyDE.Sprint5.Task6.V22/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using Tyuiu.PolyanskiyDE.Sprint5.Task6.V22.Lib;
+
+namespace Tyuiu.PolyanskiyDE.Sprint5.Task6.V22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Полянский Д. Э. | АСОиУБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                       *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнил: Полянский Данил Эдуардович | АСОиУБ-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл C: DataSprint5 InPutFileTask6V22, в котором есть набор         *");
+            Console.WriteLine("* символьных данных. Найти количество удвоенных букв ММ в заданной строке *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask6V22.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine("Количество удвоенных ММ равно " + res);
+            Console.ReadKey();
+
+        }
+    }
+}
