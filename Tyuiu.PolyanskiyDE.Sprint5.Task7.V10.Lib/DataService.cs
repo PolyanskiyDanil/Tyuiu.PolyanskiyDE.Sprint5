4a2871af6b9778d3b28f9e64ce4757c7a679e220
Tyuiu.PolyanskiyDE <danilp.2005@mail.ru>
2023-11-28 01:52:56 +0500

    Task7

---
+++ Tyuiu.PolyanskiyDE.Sprint5.Task7.V10.Lib/DataService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.PolyanskiyDE.Sprint5.Task7.V10.Lib
+{
+    public class DataService : ISprint5Task7V10
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string pathSaveFile = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask7V10.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists)
+            {
+                File.Delete(pathSaveFile);
+            }
+
+            string strLine = "";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if ((line[i] > 'A' && line[i] < 'Z'))
+                        {
+                            line = line.ToLower();
+                            strLine = strLine + line;
+                        }
+                    }
+
+                    File.AppendAllText(pathSaveFile, strLine + Environment.NewLine);
+                    strLine = "";
+                }
+                return pathSaveFile;
+            }
+        }
+    }
+}
---
+++ Tyuiu.PolyanskiyDE.Sprint5.Task7.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PolyanskiyDE.Sprint5.Task7.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PolyanskiyDE.Sprint5.Task7.V10.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c12e0c72-3d8c-4491-8fb2-03ead4159125")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
